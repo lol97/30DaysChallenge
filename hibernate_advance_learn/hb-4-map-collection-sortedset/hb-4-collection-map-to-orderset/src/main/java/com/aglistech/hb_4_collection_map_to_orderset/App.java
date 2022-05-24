8f0260f2809e16afa772a729840c11eed04d4e90
lol97 <blackmanga80@gmail.com>
2022-05-24 22:13:06 +0700

    hb-4-collection-map-to-sortedset

---
+++ hibernate_advance_learn/hb-4-map-collection-sortedset/hb-4-collection-map-to-orderset/src/main/java/com/aglistech/hb_4_collection_map_to_orderset/App.java
@@ -0,0 +1,11 @@
+package com.aglistech.hb_4_collection_map_to_orderset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
