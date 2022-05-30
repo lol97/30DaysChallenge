243e56502a3b98496561144ed779a665cd0c804a
lol97 <blackmanga80@gmail.com>
2022-05-30 21:27:55 +0700

    data type

---
+++ Springboot_Angular_MarketPlace/Learn_TypeScript/02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Rio";
+var lastName = "Halim";
+console.log(found);
+console.log("The grade is ".concat(grade));
+console.log("Hi ".concat(firstName, " ").concat(lastName));
