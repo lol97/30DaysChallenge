40ee6a5de27627d533cc1b6230b5e7d030a4c70f
lol97 <blackmanga80@gmail.com>
2022-06-01 20:55:26 +0700

    add project sales-project angular

---
+++ Springboot_Angular_MarketPlace/Learn-Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Ruby", "Permata", "ruby@aglis.com", 10000),
+    new SalesPerson("Ariawan", "", "awan@aglis.com", 20000),
+    new SalesPerson("Veny", "Siahaan", "veny@aglis.com", 1000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
