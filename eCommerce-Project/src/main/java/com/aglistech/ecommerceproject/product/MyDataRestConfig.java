f7b10142a188fc2c298e443adf56f3fbd6c21b63
lol97 <blackmanga80@gmail.com>
2022-06-03 10:24:22 +0700

    add read-only configuration

---
+++ eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/product/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.aglistech.ecommerceproject.product;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		// TODO Auto-generated method stub
+		HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+		
+		config.getExposureConfiguration()
+			.forDomainType(Product.class)
+			.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+			.withItemExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
+		
+		config.getExposureConfiguration()
+			.forDomainType(ProductCategory.class)
+			.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+			.withItemExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
+		
+	}
+
+}
