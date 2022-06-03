c8fb25c5358791b5cceab648710571c1933c710e
lol97 <blackmanga80@gmail.com>
2022-06-03 22:27:08 +0700

    mydatarest confgi

--- eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/product/MyDataRestConfig.java
+++ eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/product/MyDataRestConfig.java
@@ -18,12 +18,12 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration()
 			.forDomainType(Product.class)
 			.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-			.withItemExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
+			.withCollectionExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
 		
 		config.getExposureConfiguration()
 			.forDomainType(ProductCategory.class)
 			.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-			.withItemExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
+			.withCollectionExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
 		
 	}
 
