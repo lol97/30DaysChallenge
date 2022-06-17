d50344905a701cb6ed4c8f9c393de781b6114dfc
lol97 <blackmanga80@gmail.com>
2022-06-18 01:34:09 +0700

    add service byCategoryId

--- Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -7,12 +7,15 @@ import { Product } from '../common/product';
   providedIn: 'root'
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    //need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`; 
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     )
   }
