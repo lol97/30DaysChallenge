170f684b890c7ede34c89d7441d2aab941b5cdb3
lol97 <blackmanga80@gmail.com>
2022-06-19 19:19:42 +0700

    add endpoint findByNameContaining

--- eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/product/ProductRepository.java
+++ eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/product/ProductRepository.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "http://localhost:4200")
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
