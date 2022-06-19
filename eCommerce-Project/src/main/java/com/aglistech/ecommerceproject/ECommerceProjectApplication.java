fd7fdd3c4fed2b6e37c73cd209cbe132f14e7869
lol97 <blackmanga80@gmail.com>
2022-06-19 18:47:49 +0700

    add config log

--- eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/ECommerceProjectApplication.java
+++ eCommerce-Project/src/main/java/com/aglistech/ecommerceproject/ECommerceProjectApplication.java
@@ -1,14 +1,20 @@
 package com.aglistech.ecommerceproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class ECommerceProjectApplication {
+	private static Logger logger = LoggerFactory.getLogger(ECommerceProjectApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceProjectApplication.class, args);
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("error");
 	}
 
 }
