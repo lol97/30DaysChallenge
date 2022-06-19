ce352a7d16410bbeab7bd1c6fbe00f96b00dac5a
lol97 <blackmanga80@gmail.com>
2022-06-19 21:04:25 +0700

    add search by name

--- Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -11,20 +11,41 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  currentCategoryId: number=1;
-  currentCategoryName: string='Books';
+  currentCategoryId!: number;
+  currentCategoryName!: string;
+  searchMode!: boolean;
 
-  constructor(private productService: ProductService, 
+  constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(()=>  {
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
 
-  listProducts(){
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has("keyword");
 
+    if (this.searchMode) {
+      this.handleSearchProduct();
+    } else {
+      this.handleListProduct();
+    }
+  }
+
+  handleSearchProduct() {
+    const theKeyword: string = this.route.snapshot.paramMap.get("keyword")!;
+
+    //search for the product
+    this.productService.searchproducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProduct() {
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
 
@@ -34,6 +55,10 @@ export class ProductListComponent implements OnInit {
 
       // get "name" category from url
       this.currentCategoryName = this.route.snapshot.paramMap.get("name")!;
+    } else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
