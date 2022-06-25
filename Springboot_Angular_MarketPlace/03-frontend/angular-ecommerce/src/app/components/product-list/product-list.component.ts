3c680ead7bd1d3909c8b718ae85f9d987c292488
lol97 <blackmanga80@gmail.com>
2022-06-25 22:52:25 +0700

    service cart

--- Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -24,7 +26,8 @@ export class ProductListComponent implements OnInit {
   previousKeyword!: string;
 
   constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -111,6 +114,11 @@ export class ProductListComponent implements OnInit {
   
   addToCart(product: Product){
     console.log(`Adding to cart: ${product.name}, ${product.unitPrice}`);
+
+    // TODO ... do the real work
+    const theCartItem = new CartItem(product);
+    
+    this.cartService.addToCart(theCartItem);;
   }
 
 }
