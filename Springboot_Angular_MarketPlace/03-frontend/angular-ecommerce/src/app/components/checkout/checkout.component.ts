ffece8e8d687db960ade6e8b9a01433eba808252
lol97 <blackmanga80@gmail.com>
2024-02-11 21:28:45 +0700

    add checkout billing and shipping address

--- Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Springboot_Angular_MarketPlace/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -9,6 +9,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -17,8 +21,42 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
+  }
+
+  copyShippingAddressToBillingAddress(event : any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+            .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+    
   }
 
   onSubmit() {
